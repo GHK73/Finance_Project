44bb715343610633db6afcc771fd06dd976b0923
GHK73 <22je0371@iitism.ac.in>
2025-12-27 16:52:11 +0530

    Backend Setup

---
+++ Backend/server.js
@@ -0,0 +1,19 @@
+// backend/ server.js
+
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.get("/",(req,res)=>{
+    res.send("Trade Reconcillation & P&L System API is running");
+});
+
+app.listen(PORT,()=>{
+    console.log(`Server running on port ${PORT}`);
+});
